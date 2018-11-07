0c9305775f59dc47a858e834cce18f1e2f223ff5
Kirils_Savinihs <kirills111@gmail.com>
2018-11-07 09:55:07 +0200

    lab4 in progress

--- lab4.asm
+++ lab4.asm
@@ -1,25 +1,52 @@
-;13.var atrast videjo aritmetisko visiem para elementiem katra matricas stabina
+;13.var atrast videjo aritmetisko visiem para (pec vertibas) elementiem katra matricas stabina
 
 .model tiny
 .code
 .startup
-		Org 100h
-		jmp Short Start
-vector		Dw	2, 7, -1, 16, 15
-N		Equ	5
+        Org     100h
+        Jmp Short Start
+TROW		Equ 	2
+TCLMN 		Equ	3
+
+CLMN		Equ	2
+ROW		Equ	6
+
+Matrix		DW	1, 2, 3   
+		DW	4, 5, 6   
+CLMN_SUMS	DW	TCLMN Dup (?)
+
+S	Equ	Type Matrix
+
+;int[] clmn_sums = new int[TCLMN]
+;
+;for (int CLMN=0;i<TCLMN;i++)
+;{
+;	int clmn_sum=0
+;	for (int ROW=0; y<TROW; y++)	
+;	{
+;		if (array[i][y] % 2=0)
+;			clmn_sum+=array[i][y]
+;
+;	}
+;	clmn_sums[CLMN]=clmn_sum
+;}
 
 Start:
-		xor	bx,	bx
-		mov	cx,	N
-		mov	dx,	vector[bx]
-S:
-		mov	ax,	vector[bx]
-		cmp	dx,	ax		
-		jle	Short	cont		;if ax >= minimum (dx)
-		mov	dx,	ax		;set dx to new minimum
+	xor	bx,	bx
+	mov	cx,	TROW
+	lea	di,	CLMN_SUMS
 
-cont:
-		add	bx,	2		;i++
-		loop S
+
+for_clmns:
+	xor	ax,	ax
+	push	cx
+	jmp	for_rows
+
+
+for_rows:
+	add	ax,	Matrix[][]
+	
+
+
 .exit 0
 end
