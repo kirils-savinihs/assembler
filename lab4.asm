d6f6030a2e973b34dbdd43183f344d6e97976797
Kirils_Savinihs <kirills111@gmail.com>
2018-11-07 08:43:09 +0200

    starting lab4

---
+++ lab4.asm
@@ -0,0 +1,25 @@
+;13.var atrast videjo aritmetisko visiem para elementiem katra matricas stabina
+
+.model tiny
+.code
+.startup
+		Org 100h
+		jmp Short Start
+vector		Dw	2, 7, -1, 16, 15
+N		Equ	5
+
+Start:
+		xor	bx,	bx
+		mov	cx,	N
+		mov	dx,	vector[bx]
+S:
+		mov	ax,	vector[bx]
+		cmp	dx,	ax		
+		jle	Short	cont		;if ax >= minimum (dx)
+		mov	dx,	ax		;set dx to new minimum
+
+cont:
+		add	bx,	2		;i++
+		loop S
+.exit 0
+end
