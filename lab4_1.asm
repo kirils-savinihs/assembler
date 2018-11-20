594123f5442ab2cd6cee98bc8223e2b935a62430
Kirils_Savinihs <kirills111@gmail.com>
2018-11-20 21:53:03 +0200

    lab4 finished

---
+++ lab4_1.asm
@@ -0,0 +1,65 @@
+;13.var atrast videjo aritmetisko visiem para (pec vertibas) elementiem katra matricas stabina
+
+.model tiny
+.code
+.startup
+        Org     100h
+        Jmp Short Start
+TROW		Equ 	2
+TCLMN 		Equ	3
+
+CLMN		Equ	2
+ROW		Equ	6
+
+Matrix		DW	8, 2, 4   
+		DW	4, 4, 10   
+CLMN_AVGS	DB	TCLMN Dup (?)
+
+S	Equ	Type Matrix
+
+;int[] clmn_sums = new int[TCLMN]
+;
+;for (int CLMN=0;i<TCLMN;i++)
+;{
+;	for (int ROW=0; y<TROW; y++)	
+;	{
+;		if (array[i][y] % 2=0)
+;			clmn_sum+=array[i][y]
+;
+;	}
+;	clmn_sums[CLMN]=clmn_sum
+;}
+
+Start:
+	lea	di,	CLMN_AVGS
+	mov	cx,	TCLMN
+	xor	bx,	bx
+	xor	si,	si
+	xor	bp,	bp
+
+outer_loop:
+	push	cx				;Save what iteration outer loop is at
+	xor	ax,	ax			;Clear Ax (Used for sum)
+	xor	bx,	bx			;Clear Bx (Counts rows)
+	mov	cx,	TROW			;Set number of iterations for inner loop
+	xor	bp,	bp
+inner_loop:
+	mov	dx,	Matrix[bx][si]
+	test	dx,	1
+	jnz	odd				;If odd then skip
+	add	ax,	dx
+	inc	bp
+odd:
+	add	bx,	ROW			;Increment Bx
+	loop	inner_loop
+	
+	mov	bx,	bp
+	div	bl				;Divide Ax by Bl
+	mov	[di],	al		;Result is stored in Al, remainder in Dl
+	inc	di
+	add	si,	CLMN
+	pop	cx
+	loop	outer_loop
+
+.exit 0
+end
